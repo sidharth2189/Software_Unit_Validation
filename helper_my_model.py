1b95e33c44ef999e5c40fc28b4a2ab3cf676957c
Sidharth Das <sidharth2189@gmail.com>
2024-10-17 22:23:25 +0200

    Update comment

--- helper_my_model.py
+++ helper_my_model.py
@@ -35,6 +35,6 @@ def MyModelSim(input):
 
     return value_Y.x
 
-# Function to initialize generated code (SwuAEBDecision)
+# Function to initialize generated code
 def MyModel_Init():
     MyModelLib.my_model_initialize()
