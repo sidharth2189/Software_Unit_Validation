1d4f349c8d588af6787341c7e7d047c516ebc54d
Sidharth Das <sidharth2189@gmail.com>
2024-10-18 10:45:08 +0200

    Add sample definition for array based structure

--- interfaces/inpStr.py
+++ interfaces/inpStr.py
@@ -4,3 +4,11 @@ from interfaces.otherLib import *
 class aStruct(Structure):
 
     _fields_ = [("x", c_double)]
+
+# Example definition for array based structure
+#class bStruct(Structure):
+#
+#    _fields_ = [("x", c_double),
+#                ("y", c_double* 10),
+#                ("z", c_bool* 5),
+#        ]
